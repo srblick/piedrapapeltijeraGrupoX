721043a093b633059604a89c1fd873a8ccdf088b
Jose Quispe <josexeq@gmail.com>
2022-09-19 11:33:09 -0300

    Creado el proyecto inicial. Jose Quispe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [numero, setNumero] = useState(Math.floor(Math.random() * 3));
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Numero al azar: {numero}</h1>
+      
+      
     </div>
   );
 }
 
 export default App;
+
+
+/*Obtener un número aleatorio entre dos valores determinados
+Este ejemplo devuelve un número aleatorio situado entre dos valores específicos. 
+El valor devuelto será mayor o igual que min y menor que max.
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}*/
